e0a844d4283aec9eced10138b3a3e3ae5ada8db5
Tsur Yohananov <tsur@beatman.com>
2019-07-04 23:34:41 +0300

    stopAll()

--- androexecutor/src/main/java/com/tsuryo/androthread/AndroExecutor.java
+++ androexecutor/src/main/java/com/tsuryo/androthread/AndroExecutor.java
@@ -2,6 +2,7 @@ package com.tsuryo.androthread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Tsur Yohananov on 2019-07-04.
@@ -35,5 +36,34 @@ public class AndroExecutor {
 
     public void runOnUI(Runnable runnable) {
         mMainThreadExecutor.execute(runnable);
+    }
+
+    /**
+     * Call stopAll to stop all running tasks and shut down the ExecutorService
+     * ATTENTION: (if necessary, while/for loops)
+     * make sure to try{} catch{InterruptedException e}
+     * in your tasks and release resources if needed.
+     * <p>
+     * will cancel via Thread.interrupt, so any task that fails to respond to
+     * interrupts may never terminate.
+     */
+    public void stopAll() {
+        mService.shutdown(); // Disable new tasks from being submitted
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!mService.awaitTermination(30, TimeUnit.SECONDS)) {
+                mService.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!mService.awaitTermination(30, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            mService.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
+
+
     }
 }
